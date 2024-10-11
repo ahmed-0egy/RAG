23d4322b28fb6fb6d752d2074f12d98ef835f832
ahmed-0egy <emry741999@gmail.com>
2024-10-12 02:40:33 +0300

    Create base.py file inside routes directory, Then move the api request called "simple_api_fun" from main.py to base.py. Finally include(link) the routes in base.py to main.py.

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 from fastapi import FastAPI
+from routes import base
 
 app = FastAPI()
 
-
-"""
-   The following block of code creates a function called "simple_api_fun".
-   To make this function act as an API we add a decorator(a fastapi decorator) before it.
-   The name of the api we created is called "Welcome".
-"""
-
-@app.get("/welcome")
-def simple_api_fun():
-    return {
-        "message": "Welcome to FastAPI!"
-    }
+app.include_router(base.base_router)
