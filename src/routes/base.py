592ba98595097f0cd34ad05ecfcb7d9861c3dfc7
ahmed-0egy <emry741999@gmail.com>
2024-10-12 13:43:10 +0300

    We will make fastapi  work more efficiently by using the concept of "Depends" when we call the "get_settings" method to define "app_settings" object (You can search later for why this makes fastapi more efficient)

--- src/routes/base.py
+++ src/routes/base.py
@@ -5,8 +5,7 @@ from helpers.config import get_settings
 base_router = APIRouter(prefix="/api/v1", tags=["api_v1"])
 
 @base_router.get("/")
-async def welcome():
-    app_settings = get_settings()
+async def welcome(app_settings = Depends(get_settings)):
     app_name = app_settings.APP_NAME
     app_version = app_settings.APP_VERSION
     return {
