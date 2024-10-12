7b4679f78ca005c0989e7bb67bb4a22722ed6a29
ahmed-0egy <emry741999@gmail.com>
2024-10-12 13:15:45 +0300

    Use pydantic library to write the logic (code) that can be used to load the configuration from .env file.

---
+++ src/helpers/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+
+    APP_NAME: str
+    APP_VERSION: str
+
+    class config:
+        env_file = ".env"
+
+def get_settings():
+    return Settings()
