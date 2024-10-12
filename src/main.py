521a701479bb389e48c4358e8697e8df7d5df540
ahmed-0egy <emry741999@gmail.com>
2024-10-12 13:24:16 +0300

    Use config.py -instead of dotenv library- to load configurations (environment variables or settings) from .env file.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-from dotenv import load_dotenv
-load_dotenv(".env") # this must be before importing any other python file so that any python file can see the environment variables loaded in main.py using this line of code.
 from routes import base
 
 app = FastAPI()
--- src/routes/base.py
+++ src/routes/base.py
@@ -1,12 +1,14 @@
 import os
 from fastapi import FastAPI, APIRouter
+from helpers.config import get_settings
 
 base_router = APIRouter(prefix="/api/v1", tags=["api_v1"])
 
 @base_router.get("/")
 def welcome():
-    app_name = os.getenv("App_Name")
-    app_version = os.getenv("App_Version")
+    app_settings = get_settings()
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
     return {
         "app_name": app_name,
         "app_version": app_version
