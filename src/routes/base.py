afdde3f88b63ac1918835b8e7d3605e4cd241e3e
ahmed-0egy <emry741999@gmail.com>
2024-10-12 13:39:37 +0300

    Make the route function "welcome" an asyncronize function to make our routes more efficient (you can seaerch for the concept of syncronization to better understand this)

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,11 +1,11 @@
 import os
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends
 from helpers.config import get_settings
 
 base_router = APIRouter(prefix="/api/v1", tags=["api_v1"])
 
 @base_router.get("/")
-def welcome():
+async def welcome():
     app_settings = get_settings()
     app_name = app_settings.APP_NAME
     app_version = app_settings.APP_VERSION
